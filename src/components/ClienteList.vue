14282c18e8ff7d9663b4286fdea576776d572ae8
diegoalejandro132 <diegoalejandro132@gmail.com>
2021-07-21 11:00:26 -0400

    modificar para clientes o tutorial list

---
+++ src/components/ClienteList.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="list row">
+    <div class="col-md-8">
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Insira o CPF"
+          v-model="title"/>
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button"
+            @click="searchCPF"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <h4>Lista de clientes</h4>
+      <ul class="list-group">
+        <li class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(cliente, index) in clientes"
+          :key="index"
+          @click="setActiveCliente(cliente, index)"
+        >
+          {{ cliente.cpf }}
+        </li>
+      </ul>
+
+      <button class="m-3 btn btn-sm btn-danger" @click="removeAllClientes">
+        Remover clientes
+      </button>
+    </div>
+    <div class="col-md-6">
+      <div v-if="currentCliente">
+        <h4>Cliente</h4>
+        <div>
+          <label><strong>Nome:</strong></label> {{ currentCliente.nome }}
+        </div>
+        <div>
+          <label><strong>CPF:</strong></label> {{ currentCliente.cpf }}
+        </div>
+        <div>
+          <label><strong>Data de nascimento:</strong></label> {{ currentCliente.data_nascimento }}
+        </div>
+        <div>
+          <label><strong>Status:</strong></label> {{ currentCliente.published ? "Published" : "Pending" }}
+        </div>
+
+        <a class="badge badge-warning"
+          :href="'/tutorials/' + currentCliente.id"
+        > 
+          Edit
+        </a>
+      </div>
+      <div v-else>
+        <br />
+        <p>Selecione um cliente</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ClienteDataService from "../services/ClienteDataService";
+
+export default {
+    /* eslint-disable */
+  name: "clientes-list",
+  data() {
+    return {
+      clientes: [],
+      currentClientel: null,
+      currentIndex: -1,
+      cpf: ""
+    };
+  },
+  methods: {
+    retrieveClientes() {
+      ClienteDataService.getAll()
+        .then(response => {
+          this.clientes = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    refreshList() {
+      this.retrieveClientes();
+      this.currentCliente = null;
+      this.currentIndex = -1;
+    },
+
+    setActiveCliente(cliente, index) {
+      this.currentCliente = cliente;
+      this.currentIndex = index;
+    },
+
+    removeAllClientes() {
+      ClienteDataService.deleteAll()
+        .then(response => {
+          console.log(response.data);
+          this.refreshList();
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    
+    searchCPF() {
+      ClienteDataService.findByTitle(this.cpf)
+        .then(response => {
+          this.clientes = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    this.retrieveClientes();
+  }
+};
+</script>
+
+<style>
+.list {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -28,6 +28,11 @@ export default new Router({
       component: () => import("./components/AddCliente")
     },
     {
+      path: "/lista_cliente",
+      name: "lista de clientes",
+      component: () => import("./components/ClienteList")
+    },
+    {
       path: "/add-endereco",
       name: "addEndereco",
       component: () => import("./components/AddEndereco")
@@ -36,6 +41,6 @@ export default new Router({
       path: "/add-inquilino",
       name: "addInquilino",
       component: () => import("./components/AddInquilino")
-    },
+    }
   ]
 });
